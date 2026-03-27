d132b8b4fdecab46833f153b7e23c3b88b19813c
Sebastian Muszynski <basti@linkt.de>
2026-03-27 08:29:09 +0100

    Add missing CODEOWNERS to platform files

--- components/xiaomi_light/__init__.py
+++ components/xiaomi_light/__init__.py
@@ -2,6 +2,8 @@ import esphome.codegen as cg
 from esphome.components import light
 import esphome.config_validation as cv
 
+CODEOWNERS = ["@syssi"]
+
 xiaomi_light_ns = cg.esphome_ns.namespace("xiaomi_light")
 XiaomiLight = xiaomi_light_ns.class_("XiaomiLight", cg.Component, light.LightOutput)
 
