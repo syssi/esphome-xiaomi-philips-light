cf18e37a4a7c6e32e7b9eb9fc13609a560f1702b
Sebastian Muszynski <basti@linkt.de>
2026-05-09 06:40:34 +0200

    CI: Add C++ unit tests job (#27)

--- components/xiaomi_light/__init__.py
+++ components/xiaomi_light/__init__.py
@@ -3,6 +3,7 @@ from esphome.components import light
 import esphome.config_validation as cv
 
 CODEOWNERS = ["@syssi"]
+DEPENDENCIES = ["light", "output"]
 
 xiaomi_light_ns = cg.esphome_ns.namespace("xiaomi_light")
 XiaomiLight = xiaomi_light_ns.class_("XiaomiLight", cg.Component, light.LightOutput)
